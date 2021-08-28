6cf02853840f17bbb889d4ecf8fc974c498d57fd
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-29 02:19:50 +0530

    surya: Import XiaomiDoze from sm6150-common
    https://github.com/Wave-Project/device_xiaomi_sm6150-common/tree/r/doze

--- device.mk
+++ device.mk
@@ -50,6 +50,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Device Settings
 PRODUCT_PACKAGES += \
+    XiaomiDoze \
     XiaomiParts
 
 PRODUCT_COPY_FILES += \
