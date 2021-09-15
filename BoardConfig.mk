f36301206891d1ddb515b10eb7aaf721ddc4e9f3
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-09-15 14:48:35 +0530

    surya: Switch to clang 13 for kernel build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,6 +74,7 @@ TARGET_KERNEL_CONFIG := vendor/surya-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := r433403
 TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_ADDITIONAL_FLAGS += CONFIG_MSM_PERFORMANCE=n
