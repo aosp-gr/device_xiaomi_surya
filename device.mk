db827da526cd58c0424e9ae14babda1da83166ae
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-28 22:20:47 +0530

    Revert "[DNM]: surya: Disable FM radio for now"
    This reverts commit 01003ab76299cb0de5b411790cb709d97db521f4.

--- device.mk
+++ device.mk
@@ -108,7 +108,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # FM
-#BOARD_HAVE_QCOM_FM := true
+BOARD_HAVE_QCOM_FM := true
 
 # GPS
 LOC_HIDL_VERSION := 4.0
